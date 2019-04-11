c36a300f9eea25257e71422f7b3847084dfdd448
iRaul <rauldronk@gmail.com>
2019-04-11 13:14:12 +0300

    style changes to the search input

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,13 +3,21 @@ import styled from 'styled-components';
 const Input = styled.input`
   display: inline-block;
   height: 40px;
+  width: 100%;
+  max-width: 430px;
   line-height: 40px;
   padding: 0 14px;
   background: #fff;
   border-radius: 4px;
-  border: 0;
+  border: 1px solid transparent;
   color: rgba(0, 0, 0, 1);
   box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  transition: all 0.2s ease-out 0s;
+
+  &:focus {
+    outline: none;
+    border-color: #4facfe;
+  }
 `;
 
 export default Input;
