5d5520deb7e543b6cc5cdaed65afd44edb7536dd
Frederic Klein <frederic.klein@rwth-aachen.de>
2019-06-09 02:31:15 +0200

    Lint fixes

--- src/data/startup.js
+++ src/data/startup.js
@@ -1,160 +1,143 @@
 const startup = [
   {
-    title: "Indie Hackers",
-    description:
-      "Raw conversations with the founders behind profitable online businesses.",
-    image: "https://gdurl.com/deJh",
-    url: "https://www.indiehackers.com/podcast"
+    title: 'Indie Hackers',
+    description: 'Raw conversations with the founders behind profitable online businesses.',
+    image: 'https://gdurl.com/deJh',
+    url: 'https://www.indiehackers.com/podcast',
   },
   {
-    title: "Product Hunt Radio",
-    description:
-      "A weekly podcast with the people creating and exploring the future.",
-    url: "https://www.producthunt.com/radio"
+    title: 'Product Hunt Radio',
     rss: 'https://feeds.simplecast.com/iCV67fGr',
     rssOverride: ['description'],
+    description: 'A weekly podcast with the people creating and exploring the future.',
+    url: 'https://www.producthunt.com/radio',
   },
   {
-    title: "StartUp Podcast",
+    title: 'StartUp Podcast',
     description: "A series about what it's really like to start a business.",
-    image: "https://gdurl.com/zr7M",
-    url: "https://www.gimletmedia.com/startup/"
+    image: 'https://gdurl.com/zr7M',
+    url: 'https://www.gimletmedia.com/startup/',
   },
   {
-    title: "Rework",
-    description:
-      "A podcast by Basecamp about the better way to work and run your business.",
-    image: "https://gdurl.com/Xtgb",
-    url: "https://rework.fm/"
+    title: 'Rework',
+    description: 'A podcast by Basecamp about the better way to work and run your business.',
+    image: 'https://gdurl.com/Xtgb',
+    url: 'https://rework.fm/',
   },
   {
-    title: "How I Built This",
+    title: 'How I Built This',
     description: "Stories behind some of the world's best known companies.",
-    image: "https://gdurl.com/2mT3",
-    url: "https://www.npr.org/podcasts/510313/how-i-built-this"
+    image: 'https://gdurl.com/2mT3',
+    url: 'https://www.npr.org/podcasts/510313/how-i-built-this',
   },
   {
-    title: "Equity",
-    description:
-      "Weekly podcast focused on all things money when it comes to startups.",
-    image: "https://gdurl.com/mUo6",
-    url: "https://techcrunch.com/tag/equity-podcast/"
+    title: 'Equity',
+    description: 'Weekly podcast focused on all things money when it comes to startups.',
+    image: 'https://gdurl.com/mUo6',
+    url: 'https://techcrunch.com/tag/equity-podcast/',
   },
   {
-    title: "20 Minute VC",
-    description: "Venture Capital, Startup Funding and The Pitch.",
-    url: "http://www.thetwentyminutevc.com/"
+    title: '20 Minute VC',
     rss: 'http://thetwentyminutevc.libsyn.com/rss',
     rssOverride: ['title', 'description'],
+    description: 'Venture Capital, Startup Funding and The Pitch.',
+    url: 'http://www.thetwentyminutevc.com/',
   },
   {
-    title: "Masters of Scale",
-    description:
-      "Legendary entrepreneur Reid Hoffman tests his theories with famous founders",
-    url: "https://mastersofscale.com/"
+    title: 'Masters of Scale',
     rss: 'https://rss.art19.com/masters-of-scale',
     rssOverride: ['title', 'description'],
+    description: 'Legendary entrepreneur Reid Hoffman tests his theories with famous founders',
+    url: 'https://mastersofscale.com/',
   },
   {
-    title: "Rocketship.fm",
-    description:
-      "Essential listening for entrepreneurs and anyone working in tech today.",
-    image: "https://gdurl.com/P8kd",
-    url: "http://rocketship.fm/"
+    title: 'Rocketship.fm',
+    description: 'Essential listening for entrepreneurs and anyone working in tech today.',
+    image: 'https://gdurl.com/P8kd',
+    url: 'http://rocketship.fm/',
   },
   {
-    title: "The Growth Show",
-    description:
-      "Each episode explores the inspiring stories behind how people grow a business.",
-    image: "https://gdurl.com/4VmF",
-    url: "https://www.hubspot.com/podcasts/the-growth-show"
+    title: 'The Growth Show',
+    description: 'Each episode explores the inspiring stories behind how people grow a business.',
+    image: 'https://gdurl.com/4VmF',
+    url: 'https://www.hubspot.com/podcasts/the-growth-show',
   },
   {
-    title: "The Pitch",
-    description:
-      "Where real entrepreneurs pitch to real investors—for real money.",
-    image: "https://gdurl.com/NWMJ",
-    url: "https://www.gimletmedia.com/the-pitch"
+    title: 'The Pitch',
+    description: 'Where real entrepreneurs pitch to real investors—for real money.',
+    image: 'https://gdurl.com/NWMJ',
+    url: 'https://www.gimletmedia.com/the-pitch',
   },
   {
-    title: "Product People",
-    description:
-      "A podcast focused on great products and the people who make them.",
-    image: "https://gdurl.com/KV_Y",
-    url: "https://www.productpeople.tv/"
+    title: 'Product People',
+    description: 'A podcast focused on great products and the people who make them.',
+    image: 'https://gdurl.com/KV_Y',
+    url: 'https://www.productpeople.tv/',
   },
   {
-    title: "Y Combinator",
-    description: "A show about technology in business, research, and art.",
-    image: "https://gdurl.com/DqOk",
-    url: "https://blog.ycombinator.com/category/podcast/"
+    title: 'Y Combinator',
+    description: 'A show about technology in business, research, and art.',
+    image: 'https://gdurl.com/DqOk',
+    url: 'https://blog.ycombinator.com/category/podcast/',
   },
   {
-    title: "Build Your SaaS",
-    description:
-      "Follow Jon and Justin as they build & launch their new product: Transistor.fm",
-    image: "https://gdurl.com/j_9P",
-    url: "https://saas.transistor.fm/"
+    title: 'Build Your SaaS',
+    description: 'Follow Jon and Justin as they build & launch their new product: Transistor.fm',
+    image: 'https://gdurl.com/j_9P',
+    url: 'https://saas.transistor.fm/',
   },
   {
-    title: "The Art of the Fail",
-    description:
-      "Conversations with startups and entrepreneurs about their hardships and failures.",
-    image: "https://gdurl.com/H5tb",
-    url: "https://theaotf.com/"
-  },
-  {
-    title: "Rad Dad",
+    title: 'The Art of the Fail',
     rss: 'http://feeds.soundcloud.com/users/soundcloud:users:335201866/sounds.rss',
     rssOverride: ['description'],
     description:
-      "Stories from business leaders and intelligent parents from tech and beyond.",
-    url: "https://www.raddadshow.com/"
+      'Conversations with startups and entrepreneurs about their hardships and failures.',
+    image: 'https://gdurl.com/H5tb',
+    url: 'https://theaotf.com/',
   },
   {
-    title: "Function",
-    description:
-      "A podcast about how technology is shaping culture and communications.",
-    url: "https://glitch.com/culture/function/"
+    title: 'Rad Dad',
     rss: 'https://feeds.simplecast.com/xlj1OXXW',
     rssOverride: ['title', 'description'],
+    description: 'Stories from business leaders and intelligent parents from tech and beyond.',
+    url: 'https://www.raddadshow.com/',
   },
   {
-    title: "ETL",
-    description:
-      "Entrepreneurial stories of the secrets and setbacks behind success.",
-    image: "https://gdurl.com/BUsW",
-    url: "https://art19.com/shows/entrepreneurial-thought-leaders"
+    title: 'Function',
+    description: 'A podcast about how technology is shaping culture and communications.',
+    image: 'https://gdurl.com/bzJm',
+    url: 'https://glitch.com/culture/function/',
   },
   {
-    title: "Indie Business",
-    description: "A podcast about the process of building a podcast directory.",
-    image: "https://gdurl.com/y66M",
-    url: "https://overcast.fm/itunes1455703355/building-an-indie-business"
+    title: 'ETL',
+    description: 'Entrepreneurial stories of the secrets and setbacks behind success.',
+    image: 'https://gdurl.com/BUsW',
+    url: 'https://art19.com/shows/entrepreneurial-thought-leaders',
   },
   {
-    title: "The Podcast",
-    description:
-      "Stories from managing a bootstrapped, remote-only company (and more).",
-    image: "https://gdurl.com/RULa",
-    url: "https://thepodcast.fm"
+    title: 'Indie Business',
+    description: 'A podcast about the process of building a podcast directory.',
+    image: 'https://gdurl.com/y66M',
+    url: 'https://overcast.fm/itunes1455703355/building-an-indie-business',
   },
   {
-    title: "Schmidt List",
-    description:
-      "Interviews with people who manage, build, and lead product design teams.",
-    image: "https://gdurl.com/fuKs",
-    url: "https://www.schmidt-list.com/"
+    title: 'The Podcast',
+    description: 'Stories from managing a bootstrapped, remote-only company (and more).',
+    image: 'https://gdurl.com/RULa',
+    url: 'https://thepodcast.fm',
   },
   {
-    title: "A-Players",
-    description:
-      "Interviews with founders, venture builders, growth managers, ",
-    url: "https://anchor.fm/acelr8"
+    title: 'Schmidt List',
     rss: 'https://www.spreaker.com/show/2664825/episodes/feed',
     rssOverride: ['description'],
+    description: 'Interviews with people who manage, build, and lead product design teams.',
+    url: 'https://www.schmidt-list.com/',
   },
   {
+    title: 'A-Players',
+    description: 'Interviews with founders, venture builders, growth managers, ',
+    image: 'https://gdurl.com/O1nU',
+    url: 'https://anchor.fm/acelr8',
   },
   {
     title: 'Startup For The Rest Of Us',
@@ -163,12 +146,12 @@ const startup = [
     url: 'https://www.startupsfortherestofus.com/',
   },
   {
-    title: "Elevatd Life",
+    title: 'Elevatd Life',
     description:
-      "Seasonal themed show helping to educate folks about tech, diversity, design and more!",
-    image: "https://gdurl.com/cq9x",
-    url: "https://www.elevatdlife.com/"
-  }
+      'Seasonal themed show helping to educate folks about tech, diversity, design and more!',
+    image: 'https://gdurl.com/cq9x',
+    url: 'https://www.elevatdlife.com/',
+  },
 ];
 
 export default startup;
